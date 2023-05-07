088ff54b2cf31e4db9861f6f95b93e2f044c160b
AK-jr <132326803+AK-jr@users.noreply.github.com>
2023-05-07 09:37:57 +0530

    Update Mainbr.py

--- Mainbr.py
+++ Mainbr.py
@@ -4,31 +4,41 @@ from io import BytesIO
 import requests
 
 def welcome():
+
     
     
     welcome_window = tk.Tk()
     welcome_window.title("Welcome to Bus Reservation System")
+    welcome_window.attributes('-fullscreen', True)
 ##    welcome_window.config(bg="cyan")
     
     
     
     welcome_window.geometry("1200x800")
 
+    response = requests.get("https://edunext-main-storage-cf.edunexttechnologies.com/dalycollege/school___static/1677579944412_final_logo.jpg")
+    img = Image.open(BytesIO(response.content))
+    img = img.resize((1350,900), Image.LANCZOS)
+    test = ImageTk.PhotoImage(img)
+    bk = tk.Label(image=test)
+    bk.image = test
+    bk.place(x=0, y=0)
 
     
     
-    welcome_label = tk.Label(welcome_window, text="Welcome to Bus Reservation System!\n \n By-Aishwarya Kumar Singh\n \n XII-D\n \n Click on Enter to Continue", font=("Arial", 20))
-    welcome_label.pack(pady=50)
+    welcome_label = tk.Label(welcome_window,bg="white", text="Welcome to Bus Reservation System!\n \n By-Aishwarya Kumar Singh\n \n XII-D\n \n Click on Enter to Continue", font=("Arial", 20))
+    welcome_label.pack(pady=0)
+    
     def enter():
         welcome_window.destroy()
         home()
         
     
-    enter_button = tk.Button(welcome_window, text="Enter",padx=70,pady=15,bg="green" ,command=enter)
+    enter_button = tk.Button(welcome_window, text="Enter",padx=70,pady=15,bg="White" ,command=enter)
     enter_button.pack(pady=20)
     
-    exit_button=tk.Button(welcome_window,text="EXIT",bg="red",padx=50,pady=10,command=welcome_window.destroy)
-    exit_button.pack(pady=100)
+    exit_button=tk.Button(welcome_window,text="EXIT",bg="red",padx=30,pady=70,command=welcome_window.destroy)
+    exit_button.pack(pady=200)
     welcome_window.mainloop()
 
 
@@ -40,6 +50,16 @@ def home():
     home.geometry("1200x800")
     home.config(bg="white")
     home.title("AK TRAVEL AGENCY ")
+    home.attributes('-fullscreen', True)
+    
+    response = requests.get("https://img.freepik.com/premium-vector/bus-logo-abstract_7315-17.jpg")
+    img = Image.open(BytesIO(response.content))
+    img = img.resize((1350,800), Image.LANCZOS)
+    test = ImageTk.PhotoImage(img)
+    bk = tk.Label(image=test)
+    bk.image = test
+    bk.place(x=0, y=0)
+    
     label=tk.Label(home , text="WELCOME TO AK AGENCY",bg="red",font=("Arial",18))
     label.grid(row=0,column=7)
     global name
@@ -48,6 +68,8 @@ def home():
     global frm
     global to
     global date
+    global month
+    global year
     global company
     global mobile
     global email
@@ -59,6 +81,8 @@ def home():
     frm=tk.StringVar()
     to=tk.StringVar()
     date=tk.StringVar()
+    month=tk.StringVar()
+    year=tk.StringVar()
     company=tk.StringVar()
     mobile=tk.StringVar()
     email=tk.StringVar()
@@ -109,32 +133,46 @@ def home():
     date_label.grid(row=7, column=0)
     date_dropdown = tk.OptionMenu(home,date,"1","4","5","8","15","16","18","22","25","29","30")
     date.set("Select Date")
-    
-    ##Create dropdown as month,date,year
-
     date_dropdown.grid(row=7, column=1)
 
+
+    month_label = tk.Label(home, text="Month",bg="white")
+    month_label.grid(row=8, column=0)
+    month_dropdown = tk.OptionMenu(home,month,"January","February","March","April","May","June","July","August")
+    month.set("Select Month")
+    month_dropdown.grid(row=8, column=1)
+    
+
+    year_label = tk.Label(home, text="Year",bg="white")
+    year_label.grid(row=9, column=0)
+    year_dropdown = tk.OptionMenu(home,year,"2023","2024","2025")
+    year.set("Select Year")
+    year_dropdown.grid(row=9, column=1)
+    
+
     company_label = tk.Label(home, text="Travel Company",bg="white")
-    company_label.grid(row=8, column=0)
+    company_label.grid(row=10, column=0)
     
     company_dropdown = tk.OptionMenu(home, company, "Hans Travels", "Sharma Travels", "Verma Travels", "Chartered", "Patel Tours and Travels")
-    company_dropdown.grid(row=8, column=1)
+    company_dropdown.grid(row=10, column=1)
 
     mobile_label = tk.Label(home, text="Mobile Number",bg="white")
-    mobile_label.grid(row=9, column=0)
+    mobile_label.grid(row=11, column=0)
     mobile_entry = tk.Entry(home,textvariable=mobile)
-    mobile_entry.grid(row=9, column=1)
+    mobile_entry.grid(row=11, column=1)
+    mobile.set("+91")
 
     email_label = tk.Label(home, text="Email",bg="white")
-    email_label.grid(row=10, column=0)
+    email_label.grid(row=12, column=0)
     email_entry = tk.Entry(home,bg="white",textvariable=email)
-    email_entry.grid(row=10, column=1)
+    email_entry.grid(row=12, column=1)
+    email.set("  @gmail.com")
 
     seat_label = tk.Label(home, text="Seat Number",bg="white")
-    seat_label.grid(row=11, column=0)
+    seat_label.grid(row=13, column=0)
     seat_dropdown=tk.OptionMenu(home, seat, "4", "7", "12", "15", "16","19","24")
     seat.set("Select your Seat Number")
-    seat_dropdown.grid(row=11, column=1)
+    seat_dropdown.grid(row=13, column=1)
     
 
     
@@ -144,7 +182,7 @@ def home():
     
     
     change=tk.Button(home,text="SUBMIT",bg="cyan",padx=70,pady=10,command=switch)
-    change.grid(row=12, column=2)
+    change.grid(row=15, column=2)
 
 ##    submit_button = tk.Button(home, text="Submit", command=submit)
 ##    submit_button.grid(row=11, column=0,)
