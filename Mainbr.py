776d77ba7b241d1d7f97b6563432f60267930979
AK-jr <132326803+AK-jr@users.noreply.github.com>
2023-05-09 22:15:03 +0530

    Update Mainbr.py

--- Mainbr.py
+++ Mainbr.py
@@ -5,17 +5,13 @@ import requests
 
 def welcome():
 
-    
-    
+    #Creation of Welcome Window
     welcome_window = tk.Tk()
     welcome_window.title("Welcome to Bus Reservation System")
     welcome_window.attributes('-fullscreen', True)
-##    welcome_window.config(bg="cyan")
-    
-    
-    
     welcome_window.geometry("1200x800")
-
+    
+    #Applying background Image
     response = requests.get("https://edunext-main-storage-cf.edunexttechnologies.com/dalycollege/school___static/1677579944412_final_logo.jpg")
     img = Image.open(BytesIO(response.content))
     img = img.resize((1350,900), Image.LANCZOS)
@@ -25,14 +21,14 @@ def welcome():
     bk.place(x=0, y=0)
 
     
-    
+    #putting text in Welcom window
     welcome_label = tk.Label(welcome_window,bg="white", text="Welcome to Bus Reservation System!\n \n By-Aishwarya Kumar Singh\n \n XII-D\n \n Click on Enter to Continue", font=("Arial", 20))
     welcome_label.pack(pady=0)
     
     def enter():
-        welcome_window.destroy()
+        welcome_window.destroy()    #defining function to switch windows
         home()
-        
+                                            
     
     enter_button = tk.Button(welcome_window, text="Enter",padx=70,pady=15,bg="White" ,command=enter)
     enter_button.pack(pady=20)
@@ -45,7 +41,7 @@ def welcome():
 def home():
     
     
-    
+    #Creating New Window For inputting Ticket Details
     home = tk.Tk()
     home.geometry("1200x800")
     home.config(bg="white")
@@ -93,7 +89,7 @@ def home():
         
     def switch():
         home.destroy()
-        ticket()
+        thankyou()
 
 
     name_label = tk.Label(home, text="Name",bg="white")
@@ -116,15 +112,15 @@ def home():
 
     frm_label = tk.Label(home, text="from",bg="white")
     
-    frm_label.grid(row=5, column=0)    # 
-##    frm_entry = tk.Entry(home,textvariable=frm)
+    frm_label.grid(row=5, column=0)    
+
     From_dropdown = tk.OptionMenu(home, frm ,"DELHI","MUMBAI","JAIPUR","NAGPUR","AGRA","INDORE")
     frm.set("Select A Boarding City")
     From_dropdown.grid(row=5, column=1)    
 
     to_label = tk.Label(home, text="To",bg="white")
     to_label.grid(row=6, column=0)
-##    to_entry = tk.Entry(home,textvariable=to)
+
     to_dropdown = tk.OptionMenu(home, to ,"DELHI","MUMBAI","JAIPUR","NAGPUR","AGRA","INDORE")
     to.set("Select Your Destination")
     to_dropdown.grid(row=6, column=1)
@@ -184,29 +180,31 @@ def home():
     change=tk.Button(home,text="SUBMIT",bg="cyan",padx=70,pady=10,command=switch)
     change.grid(row=15, column=2)
 
-##    submit_button = tk.Button(home, text="Submit", command=submit)
-##    submit_button.grid(row=11, column=0,)
+
     home.mainloop()
 
-def ticket():
-    ticket=tk.Tk()
-    ticket.geometry("1200x800")
+#window for thanking customer For choosing us     
+
+def thankyou():
+    thx_window = tk.Tk()
+    thx_window.title("Thank you")
+    thx_window.config(bg="white")
+    thx_window.attributes('-fullscreen', True)    
+    thx_window.geometry("1200x800")
+    response = requests.get("https://img.freepik.com/premium-vector/bus-logo-abstract_7315-17.jpg")
+    img = Image.open(BytesIO(response.content))
+    img = img.resize((1350,800), Image.LANCZOS)
+    test = ImageTk.PhotoImage(img)
+    bk = tk.Label(image=test)
+    bk.image = test
+    bk.place(x=0, y=0)
+    Thx_label= tk.Label(thx_window,bg="white", text="THANK YOU FOR TRUSTING AK AGENCY\n\nYOUR TICKET WILL BE SEND ON YOUR\nENTERED MAIL AND PHONE NUMBER\n\n TOLL FREE NUMBER 1800 XXXXXX", font=("Arial", 20))
+    Thx_label.pack(pady=100)
     
-    inp=tk.Text(ticket,width=1200,height=800,font="Arial 20")
-    inp.pack(pady = 50)
-##    ticket.config(bg="black")
-##    inp.insert(INSERT,name.get())
-##    inp.insert(INSERT,age.get())
-##    inp.insert(INSERT,gender.get())
-##    inp.insert(INSERT,gender.get())
-##    inp.insert(INSERT,frm.get())
-##    inp.insert(INSERT,to.get())
-##    inp.insert(INSERT,date.get())
-##    inp.insert(INSERT,comapany.get())
-##    inp.insert(INSERT,mobile.get())
-##    inp.insert(INSERT,email.get())
-##    inp.insert(INSERT,seat.get())
-    
-    ticket.mainloop()
+    exitthx_button=tk.Button(thx_window, text="EXIT",bg="red",padx=70,pady=10,command=thx_window.destroy)
+    exitthx_button.pack(pady=150)
+
+
+#Calling function to Start The App    
 welcome()    
-##home()
+
